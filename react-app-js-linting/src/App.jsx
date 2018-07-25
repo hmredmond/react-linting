9a2e71a180469769310c3ab9d3282b9e9e2a04be
Hannah Redmond <hannah.redmond@baesystemsdetica.com>
2018-07-25 15:10:19 +0100

    updated commanded in package.json

--- react-app-js-linting/src/App.jsx
+++ react-app-js-linting/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
-import {withStyles} from '@material-ui/core/styles';
-import {LinearProgress} from '@material-ui/core/LinearProgress';
-import {Button} from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
+import { LinearProgress } from '@material-ui/core/LinearProgress';
+import { Button } from '@material-ui/core/Button';
 import logo from './logo.svg';
 
 const styles = {
-	root: {
-		flexGrow: 1
-	}
+  root: {
+    flexGrow: 1
+  }
 };
 
 /*
@@ -18,40 +18,50 @@ const styles = {
  * @extends {React.Component}
  */
 class App extends React.Component {
-	componentDidMount() {
-		this.timer = setInterval(this.progress, 500);
-	}
 
-	/**
-	 * Component will unmount
-	 *
-	 * @memberof App
-	 */
-	componentWillUnmount() {
-		clearInterval(this.timer);
-	}
-	/**
-	 *
-	 * progress
-	 * @memberof App
-	 */
-	progress = () => {
-		const {completed} = this.state;
-		if (completed === 100) {
-			this.setState({
-				completed: 0
-			});
-		} else {
-			const diff = Math.random() * 10;
-			this.setState({
-				completed: Math.min(completed + diff, 100)
-			});
-		}
-	};
+  /**
+   *
+   * Component did mount
+   * @memberof App
+   */
+  componentDidMount() {
+    this.timer = setInterval(this.progress, 500);
+  }
 
-	render() {
-		return (
-			<div className="App">
+  /**
+   * Component will unmount
+   *
+   * @memberof App
+   */
+  componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+  /**
+   *
+   * progress
+   * @memberof App
+   */
+  progress = () => {
+    const {completed} = this.state;
+    if (completed === 100) {
+      this.setState({
+        completed: 0
+      });
+    } else {
+      const diff = Math.random() * 10;
+      this.setState({
+        completed: Math.min(completed + diff, 100)
+      });
+    }
+  };
+  /**
+   * Component will unmount
+   *
+   * @memberof App
+   */
+  render() {
+    return (
+      <div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">
@@ -70,17 +80,17 @@ class App extends React.Component {
 					<LinearProgress variant="determinate" value={this.state.completed} />
 				</div>
 			</div>
-		);
-	}
+      );
+  }
 }
 
 App.propTypes = {
-	/**
-	 * An isRequired prop that should not be visible in the documentation.
-	 *
-	 * @ignore
-	 */
-	classes: PropTypes.object.isRequired
+  /**
+   * An isRequired prop that should not be visible in the documentation.
+   *
+   * @ignore
+   */
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(App);
--- react-app-js-linting/src/index.js
+++ react-app-js-linting/src/index.js
@@ -5,5 +5,4 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App classes="{something : boo}" />, document.getElementById('root'));
-
 registerServiceWorker();
